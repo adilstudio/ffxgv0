a53920c67492c76d4e24bf6a254bd718c69639ba
Adil Boujibar <contact@adilstudio.com>
2023-02-14 03:02:09 +0100

    docker compose conf and scaffold player

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,43 @@
 # Use the Ruby 2.7 image as a base
-FROM ruby:2.7
+FROM ruby:2.7.0-alpine as builder
 
 # Set the working directory
-WORKDIR /app
+WORKDIR /ffgx
+
+RUN gem install bundler -v 2.3.20 --no-document
+
+# Set local timezone
+RUN apk add --update tzdata
+
+RUN --mount=type=cache,id=apk,sharing=locked,target=/etc/apk/cache \
+  apk add build-base imagemagick-dev imagemagick git autoconf automake libtool pkgconfig
+
+RUN apk --no-cache add git npm mariadb-dev nodejs yarn openjdk8-jre mysql-client curl python3 python3-dev py3-pip openssl build-base && \
+  cp /usr/share/zoneinfo/Europe/Paris /etc/localtime && \
+  echo "Europe/Paris" > /etc/timezone
 
 # Copy the Gemfile and Gemfile.lock
-COPY Gemfile Gemfile.lock ./
+COPY Gemfile Gemfile.lock /ffgx/
 
 # Install the gems
 RUN bundle install
+
+RUN adduser -D bundler
+
+USER root
+
+RUN --mount=type=cache,target=/home/bundler/bundle,id=bundler \
+  chown bundler: -R /home/bundler/bundle
+
+
+USER bundler
+
 # --without development test
 
-# Copy the rest of the application code
-COPY . .
+COPY --chown=bundler:bundler . /ffxg/
+
+USER root
+RUN chown -R 1000:1000 /ffgx
 
 # Set the default command to run the Rails server
-CMD ["bundle", "exec", "rails", "server", "-b", "0.0.0.0"]
+#CMD ["bundle", "exec", "rails", "server", "-b", "0.0.0.0"]
---
+++ docker/entrypoint-init.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+echo "Creating directories."
+mkdir -p "/ffxg/instances/$DB_NAME/dbpassword/"
+mkdir -p "/ffxg/instances/$DB_NAME/matomo/"
+if [ ! -d "/ffxg/instances/$DB_NAME/production/" ]; then
+    mkdir -p "/ffxg/instances/$DB_NAME/production/"
+    chown 1000:1000 -R "/ffxg/instances/$DB_NAME/production/"
+fi
+if [ ! -d "/ffxg/instances/$DB_NAME/sidekiq/" ]; then
+    mkdir -p "/ffxg/instances/$DB_NAME/sidekiq/"
+    chown 1000:1000 -R "/ffxg/instances/$DB_NAME/sidekiq/"
+fi
+echo "Done!"
+
+DB_PASSWORD_FILE="/ffxg/instances/$DB_NAME/dbpassword/dbpassword"
+
+if [ "$(mysql -BN -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = \"$DB_USER\")")" = "0" ]; then
+    echo "MySQL user $DB_NAME doesn't exist."
+    if [ -e "$DB_PASSWORD_FILE" ]; then
+        echo "Loading MySQL password from $DB_PASSWORD_FILE"
+        DB_PASSWORD=$(cat "$DB_PASSWORD_FILE")
+    else
+        echo "Generating MySQL password"
+        DB_PASSWORD=$(apg -n1 -m16 -MNCL)
+        echo "$DB_PASSWORD" > "$DB_PASSWORD_FILE"
+    fi
+    echo "Creating MySQL user $DB_NAME"
+    echo $DB_PASSWORD
+    mysql -e "CREATE USER \"$DB_USER\"@\"localhost\" IDENTIFIED BY \"$DB_PASSWORD\""
+    echo "Done!"
+else
+    echo "MySQL user $DB_USER exists."
+fi
+if [ "$(mysql -BN -e "SELECT EXISTS(SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = \"$DB_NAME\");")" = 0 ]; then
+    echo "Creating MySQL database $DB_NAME"
+    mysql -e "CREATE DATABASE \`$DB_NAME\`"
+    echo "Done!"
+else
+    echo "MySQL database $DB_NAME exists."
+fi
+echo "Granting/fixing privileges"
+mysql -e "GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO \"$DB_USER\"@\"localhost\""
+echo "Done!"
