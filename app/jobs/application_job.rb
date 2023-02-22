8a3186bd648ab449fd320d3338a301e7ef76b0ec
Adil Boujibar <contact@adilstudio.com>
2023-02-22 23:38:08 +0100

    fixing stuff

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -4,4 +4,24 @@ class ApplicationJob < ActiveJob::Base
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
+=begin
+  require 'uri'
+  require 'net/http'
+  require 'openssl'
+
+  def call_api(endpoint)
+    url = URI("https://v3.football.api-sports.io/{endpoint}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-host"] = 'v3.football.api-sports.io'
+    request["x-rapidapi-key"] = 'XxXxXxXxXxXxXxXxXxXxXxXx'
+
+    response = http.request(request)
+    puts response.read_body
+  end
+=end
 end
